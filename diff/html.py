56357942cb47b73291e56812a826ef7207077a1f
Omar Aflak <aflakomar@gmail.com>
2022-06-12 19:03:38 +0100

    clean

--- diff/html.py
+++ diff/html.py
@@ -1,7 +1,15 @@
 from typing import Iterable, TypeVar
 from diff.myers import diff, Operation
 
-T = TypeVar("T")
+T = TypeVar('T')
+
+def create_html_diff(s1: Iterable[T], s2: Iterable[T]) -> str:
+    result = '<pre>'
+    for edit in diff(s1, s2):
+        for data in edit.data:
+            result += _format_data(data, edit.operation)
+    result += '</pre>'
+    return result
 
 def _format_data(data: T, operation: Operation) -> str:
     if operation == Operation.KEEP:
@@ -11,11 +19,3 @@ def _format_data(data: T, operation: Operation) -> str:
     if operation == Operation.DELETE:
         return f'<span style="background-color: #FFCDD2;">{data}</span>'
     raise RuntimeError(f'Did not recognize operation {operation}')
-
-def create_html_diff(s1: Iterable[T], s2: Iterable[T]) -> str:
-    result = '<pre>'
-    for edit in diff(s1, s2):
-        for data in edit.data:
-            result += _format_data(data, edit.operation)
-    result += '</pre>'
-    return result
